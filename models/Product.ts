4ddeae2051597d7548c06f655f8529e2450e6abd
astroveda <astroplatformmm@gmail.com>
2026-05-10 12:48:44 +0530

    Update Product.ts

--- models/Product.ts
+++ models/Product.ts
@@ -6,6 +6,7 @@ const productSchema = new Schema(
     title: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
+    mrp: { type: Number, default: null },
     image: { type: String, required: true },
     images: { type: [String], default: [] },
     options: {
