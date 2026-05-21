dbd50bccedb62095fab0dc6c31f752083e725252
astroveda <astroplatformmm@gmail.com>
2026-05-21 20:04:02 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -99,6 +99,18 @@ export default function AboutPage() {
     "/awards1/Prerna Award.jpg",
     "/awards1/Tamas Global Awards Certificate.jpg",
     "/awards1/WhatsApp Image 2025-01-26 at 3.55.32 PM.jpeg",
+    "/awards1/A01_JPG-Copy.jpeg",
+    "/awards1/A02_JPG-Copy.jpeg",
+    "/awards1/A03_JPG1-Copy.jpeg",
+    "/awards1/A04_JPG-Copy.jpeg",
+    "/awards1/A05_JPG-Copy.jpeg",
+    "/awards1/A06_JPG-Copy.jpeg",
+    "/awards1/A07_JPG-Copy.jpeg",
+    "/awards1/A08_JPG-Copy.jpeg",
+    "/awards1/A09_JPG-Copy.jpeg",
+    "/awards1/A10_JPG-Copy.jpeg",
+    "/awards1/A11_JPG-Copy.jpeg",
+    "/awards1/A12_JPG-Copy.jpeg",
   ];
 
   const awardsPhotos = [
@@ -129,18 +141,6 @@ export default function AboutPage() {
     "/awards/WhatsApp Image 2026-04-27 at 22.37.25.jpeg",
     "/awards/WhatsApp Image 2026-04-27 at 22.37.30.jpeg",
     "/awards/_J0A6182.JPG",
-    "/awards1/A01_JPG-Copy.jpeg",
-    "/awards1/A02_JPG-Copy.jpeg",
-    "/awards1/A03_JPG1-Copy.jpeg",
-    "/awards1/A04_JPG-Copy.jpeg",
-    "/awards1/A05_JPG-Copy.jpeg",
-    "/awards1/A06_JPG-Copy.jpeg",
-    "/awards1/A07_JPG-Copy.jpeg",
-    "/awards1/A08_JPG-Copy.jpeg",
-    "/awards1/A09_JPG-Copy.jpeg",
-    "/awards1/A10_JPG-Copy.jpeg",
-    "/awards1/A11_JPG-Copy.jpeg",
-    "/awards1/A12_JPG-Copy.jpeg",
   ];
 
   return (
